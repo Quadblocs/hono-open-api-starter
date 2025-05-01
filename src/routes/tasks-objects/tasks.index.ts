d9003b3a4a3bc04e122f8e4cada16fda6fd781f7
Joshua Bernstein <joshua.bernstein@outlook.com>
2025-04-30 21:54:10 -0600

    Converted the task endpoints to use objects with mapped types

---
+++ src/routes/tasks-objects/tasks.index.ts
@@ -0,0 +1,21 @@
+import { createRouter } from "@/lib/create-app";
+
+import { handlers } from "./tasks.handlers";
+import { routes } from "./tasks.routes";
+
+// This is the old way and works fine, but has to be manually updated when a route method is added
+const router = createRouter()
+  .openapi(routes.list, handlers.list)
+  .openapi(routes.create, handlers.create)
+  .openapi(routes.getOne, handlers.getOne)
+  .openapi(routes.patch, handlers.patch)
+  .openapi(routes.remove, handlers.remove);
+
+// I want to do something like this to loop through all of the methods and then get the types.
+// It probably needs a workaround, similar to how you did the "export type AppType = typeof routes[number];" for the main app.
+const typedRouter = createRouter();
+for (const route of Object.keys(routes) as (keyof typeof routes)[]) {
+  typedRouter.openapi(routes[route], handlers[route]);
+}
+
+export default router;
